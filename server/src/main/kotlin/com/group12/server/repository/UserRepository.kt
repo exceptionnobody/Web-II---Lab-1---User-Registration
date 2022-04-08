35181158446ad691a4c2b42d478f472177349b92
Marco Ballario <marco.ballario1997@gmail.com>
2022-04-08 18:45:09 +0200

    Add ActivationRepository and UserRepository

---
+++ server/src/main/kotlin/com/group12/server/repository/UserRepository.kt
@@ -0,0 +1,9 @@
+package com.group12.server.repository
+
+import com.group12.server.entity.User
+import org.springframework.data.repository.CrudRepository
+
+interface UserRepository : CrudRepository<User, Long> {
+    fun findByEmail() : User
+
+}
